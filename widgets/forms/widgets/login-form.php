b167393afb9ac9cbb545af6773179ed65342af2b
Cesar Correchel <cesarcorrechel@gmail.com>
2019-12-22 21:34:06 -0600

    add label and button controls

--- widgets/forms/widgets/login-form.php
+++ widgets/forms/widgets/login-form.php
@@ -24,4 +24,48 @@ class Login_Form extends Widget_Base{
         return ['elemental-membership-category'];
     }
 
+    protected function _register_controls(){
+
+        $this->start_controls_section(
+            'em_login_fields_section',
+            [
+                'label' => __( 'Fields', 'elemental-membership' ),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        $this->add_control(
+            'em_login_show_labels',
+            [
+                'label' => __('Show Label', 'elemental-membership'),
+                'type' => Controls_Manager::SWITCHER,
+                'return_value' => 'true',
+                'default' => '',
+            ]
+        );
+
+        $this->end_controls_section();
+
+        $this->start_controls_section(
+            'em_login_button_section',
+            [
+                'label' => __( 'Login Button', 'elemental-membership' ),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        $this->add_control(
+            'em_login_button_text',
+            [
+                'label' => __( 'Button Text', 'elemental-membership'),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'default' => 'Login',
+                'placeholder' => ''
+            ]
+        );
+
+        $this->end_controls_section();
+
+    }
+
 }
