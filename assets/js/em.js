4d168dd0c1385e4e5baf262a8745b8c78d47e947
Cesar Correchel <cesarcorrechel@gmail.com>
2019-12-30 23:23:23 -0600

    add js asset

---
+++ assets/js/em.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($) {
+  $(".em-user-registration-form").on("submit", function(e) {
+    e.preventDefault();
+    var form = $(this);
+
+    $.ajax({
+      url: em_ajax.ajax_url,
+      type: "POST",
+      data: form.serialize(),
+      success: () => {
+        console.log("this was successful");
+        $(this)[0].reset();
+      },
+
+      error: (xhr, status) => {
+        console.log(xhr.responseText);
+        console.log("status: " + status);
+      }
+    });
+    
+  });
+});
