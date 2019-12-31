dd174652e16d5a9e5fa17a2b3fcd54e80c8664a0
Cesar Correchel <cesarcorrechel@gmail.com>
2019-12-31 10:16:17 -0600

    add rudimentary wp user registration class

---
+++ widgets/forms/classes/register-user.php
@@ -0,0 +1,56 @@
+<?php
+namespace ElementalMembership\Widgets\Forms\Classes;
+
+class Register_User{
+
+    public function __construct(){
+        add_action('wp_ajax_em_register_user', [$this, 'em_register_user']);
+        add_action('wp_ajax_nopriv_em_register_user', [$this, 'em_register_user']);
+    }
+
+
+     /**
+	 * EM Register USer
+	 *
+	 * Register new user in Wordpress
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
+    public function em_register_user(){
+
+        $user_login = '';
+        $user_email = '';
+        $user_password = '';
+
+        foreach($_POST['form_fields'] as $field => $value):
+            if($field == "username"):
+                $user_login = $value;
+            elseif($field == "password"):
+                $user_password = $value;
+            elseif($field == "your-email"):
+                $user_email = $value;
+            endif;
+
+        endforeach;
+
+
+        if (isset($user_login) && isset($user_password) && isset($user_email)):
+
+            $userdata = array(
+                'user_login' => $user_login,
+                'user_pass' => $user_password,
+                'user_email' => $user_email,
+            );
+
+            wp_insert_user($userdata);
+
+        endif;
+
+        wp_die();
+
+    }
+
+}
+
+new Register_User();
