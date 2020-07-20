e2251b11c02f16bdf9d945a615ab705da1877a5c
Cesar Correchel <cesarcorrechel@gmail.com>
2020-07-19 21:24:14 -0600

    add settings page template/view

--- admin/admin.php
+++ admin/admin.php
@@ -9,21 +9,46 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Admin {
 
     public function __construct(){
-        add_action('admin_menu', array($this,'admin_menu'));
+        add_action('admin_menu', [$this,'em_admin_menu']);
     }
 
-    public function admin_menu(){
+    public function em_admin_menu(){
 
         add_menu_page(
             __('Elemental Membership', 'elemental-membership'),
             __('Elemental Membership', 'elemental-membership'),
             'manage_options',
             'elemental-membership',
-            '',
+            [$this, 'em_settings_page'],
             'dashicons-menu-alt',
             60
         );
 
+        add_submenu_page(
+            'elemental-membership', 
+            __('Settings', 'elemental-membership'), 
+            __('Settings', 'elemental-membership'),
+            'manage_options',
+            'elemental-membership',
+            [$this, 'em_settings_page']
+        );
+
+        add_submenu_page(
+            'elemental-membership', 
+            __('Get Help', 'elemental-membership'), 
+            __('Get Help', 'elemental-membership'),
+            'manage_options',
+            'em-help',
+            [$this, 'em_get_help_page']
+        );
+    
+    }
+
+    function em_settings_page(){
+        include( EM_DIR_PATH . '/admin/templates/settings-temp.php' );
+    }
+
+    function em_get_help_page(){
     }
 
 }
---
+++ admin/templates/settings-temp.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+$default_tab = null;
+$tab = isset($_GET['tab']) ? $_GET['tab'] : $default_tab;
+
+?>
+
+<div class="wrap">
+
+    <h1><?php echo get_admin_page_title(); ?></h1>
+
+    <nav class="nav-tab-wrapper em-nav-tab">
+      <a href="?page=elemental-membership" class="nav-tab <?php if ($tab === null): ?>nav-tab-active<?php endif;?>">General</a>
+      <a href="?page=elemental-membership&tab=registration" class="nav-tab <?php if ($tab === 'registration'): ?>nav-tab-active<?php endif;?>">Registration</a>
+      <a href="?page=elemental-membership&tab=login" class="nav-tab <?php if ($tab === 'login'): ?>nav-tab-active<?php endif;?>">Login</a>
+    </nav>
+
+    <div class="tab-content">
+
+    </div>
+
+</div>
