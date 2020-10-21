8a6c76922980800c86d26ce4834452495f2560e5
Cesar Correchel <cesarcorrechel@gmail.com>
2020-10-20 21:49:16 -0600

    add visibility opts for actions and logout

--- widgets/profile-header/profile-header.php
+++ widgets/profile-header/profile-header.php
@@ -2,10 +2,10 @@
 
 namespace ElementalMembership\Widgets\ProfileHeader;
 
-use Elementor\Widget_Base;
 use Elementor\Controls_Manager;
 use Elementor\Core\Schemes;
 use Elementor\Group_Control_Typography;
+use Elementor\Widget_Base;
 
 class Profile_Header extends Widget_Base{
 
@@ -30,237 +30,249 @@ class Profile_Header extends Widget_Base{
         $this->start_controls_section(
             'em_profile_header_section',
             [
-                'label' => __( 'Profile Header', 'elemental-membership' ),
+                'label' => __('Profile Header', 'elemental-membership'),
                 'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
             ]
         );
 
-		$this->add_control(
-			'show_profile_action_menu',
-			[
-				'label' => __( 'Show Action Menu', 'elemental-membership' ),
-				'type' => \Elementor\Controls_Manager::SWITCHER,
-				'label_on' => __( 'Show', 'elemental-membership' ),
-				'label_off' => __( 'Hide', 'elemental-membership' ),
-				'return_value' => 'yes',
-				'default' => 'yes',
-			]
-		);
+        $this->add_control(
+            'show_profile_action_menu',
+            [
+                'label' => __('Show Action Menu', 'elemental-membership'),
+                'type' => \Elementor\Controls_Manager::SWITCHER,
+                'label_on' => __('Show', 'elemental-membership'),
+                'label_off' => __('Hide', 'elemental-membership'),
+                'return_value' => 'yes',
+                'default' => 'yes',
+            ]
+        );
+
+        $this->add_control(
+            'show_logout_link',
+            [
+                'label' => __('Logout Link', 'elemental-membership'),
+                'type' => \Elementor\Controls_Manager::SWITCHER,
+                'label_on' => __('Show', 'elemental-membership'),
+                'label_off' => __('Hide', 'elemental-membership'),
+                'return_value' => 'yes',
+                'default' => 'yes',
+            ]
+        );
 
         $this->end_controls_section();
 
         $this->start_controls_section(
             'em_profile_banner_section',
             [
-                'label' => __( 'Profile Banner', 'elemental-membership' ),
+                'label' => __('Profile Banner', 'elemental-membership'),
                 'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
             ]
         );
 
         $this->add_control(
-			'profile_banner_image',
-			[
-				'label' => __( 'Choose Image', 'elemental-membership' ),
-				'type' => \Elementor\Controls_Manager::MEDIA,
-				'default' => [
-					'url' => \Elementor\Utils::get_placeholder_image_src(),
-				],
-			]
+            'profile_banner_image',
+            [
+                'label' => __('Choose Image', 'elemental-membership'),
+                'type' => \Elementor\Controls_Manager::MEDIA,
+                'default' => [
+                    'url' => \Elementor\Utils::get_placeholder_image_src(),
+                ],
+            ]
         );
 
         $this->add_control(
-			'profile_banner_height',
-			[
-				'label' => __( 'Banner Height', 'elemental-membership' ),
-				'type' => Controls_Manager::SLIDER,
-				'size_units' => [ 'px', '%' ],
-				'range' => [
-					'px' => [
-						'min' => 100,
-						'max' => 1000,
-						'step' => 5,
-					],
-					'%' => [
-						'min' => 15,
-						'max' => 100,
-					],
-				],
-				'default' => [
-					'unit' => 'px',
-					'size' => 200,
-				],
-				'selectors' => [
-					'{{WRAPPER}} .em-profile-banner__bg' => 'height: {{SIZE}}{{UNIT}};',
-				],
-			]
-		);
-        
+            'profile_banner_height',
+            [
+                'label' => __('Banner Height', 'elemental-membership'),
+                'type' => Controls_Manager::SLIDER,
+                'size_units' => ['px', '%'],
+                'range' => [
+                    'px' => [
+                        'min' => 100,
+                        'max' => 1000,
+                        'step' => 5,
+                    ],
+                    '%' => [
+                        'min' => 15,
+                        'max' => 100,
+                    ],
+                ],
+                'default' => [
+                    'unit' => 'px',
+                    'size' => 200,
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .em-profile-banner__bg' => 'height: {{SIZE}}{{UNIT}};',
+                ],
+            ]
+        );
+
         $this->end_controls_section();
 
         $this->start_controls_section(
             'profile_image',
             [
-                'label' => __( 'Profile Image', 'elemental-membership' ),
+                'label' => __('Profile Image', 'elemental-membership'),
                 'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
             ]
         );
 
         $this->add_control(
-			'profile_user_image_width',
-			[
-				'label' => __( 'Profile Image Width', 'elemental-membership' ),
-				'type' => Controls_Manager::SLIDER,
-				'size_units' => [ 'px', '%' ],
-				'range' => [
-					'px' => [
-						'min' => 80,
-						'max' => 1000,
-						'step' => 5,
-					],
-					'%' => [
-						'min' => 15,
-						'max' => 100,
-					],
-				],
-				'default' => [
-					'unit' => 'px',
-					'size' => 150,
-				],
-				'selectors' => [
-					'{{WRAPPER}} .em-user-avatar img' => 'width: {{SIZE}}{{UNIT}};',
-				],
-			]
+            'profile_user_image_width',
+            [
+                'label' => __('Profile Image Width', 'elemental-membership'),
+                'type' => Controls_Manager::SLIDER,
+                'size_units' => ['px', '%'],
+                'range' => [
+                    'px' => [
+                        'min' => 80,
+                        'max' => 1000,
+                        'step' => 5,
+                    ],
+                    '%' => [
+                        'min' => 15,
+                        'max' => 100,
+                    ],
+                ],
+                'default' => [
+                    'unit' => 'px',
+                    'size' => 150,
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .em-user-avatar img' => 'width: {{SIZE}}{{UNIT}};',
+                ],
+            ]
         );
 
         $this->add_control(
-			'profile_user_image_radius',
-			[
-				'label' => __( 'Profile Image Radius', 'elemental-membership' ),
-				'type' => Controls_Manager::SLIDER,
-				'size_units' => [ 'px', '%' ],
-				'range' => [
-					'px' => [
-						'min' => 80,
-						'max' => 1000,
-						'step' => 5,
-					],
-					'%' => [
-						'min' => 15,
-						'max' => 100,
-					],
-				],
-				'default' => [
-					'unit' => 'px',
-					'size' => 150,
-				],
-				'selectors' => [
-					'{{WRAPPER}} .em-user-avatar img' => 'border-radius: {{SIZE}}{{UNIT}};',
-				],
-			]
+            'profile_user_image_radius',
+            [
+                'label' => __('Profile Image Radius', 'elemental-membership'),
+                'type' => Controls_Manager::SLIDER,
+                'size_units' => ['px', '%'],
+                'range' => [
+                    'px' => [
+                        'min' => 80,
+                        'max' => 1000,
+                        'step' => 5,
+                    ],
+                    '%' => [
+                        'min' => 15,
+                        'max' => 100,
+                    ],
+                ],
+                'default' => [
+                    'unit' => 'px',
+                    'size' => 150,
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .em-user-avatar img' => 'border-radius: {{SIZE}}{{UNIT}};',
+                ],
+            ]
         );
-        
+
         $this->add_control(
-			'profile_user_image_padding',
-			[
-				'label' => __( 'Image Padding', 'elemental-membership' ),
-				'type' => Controls_Manager::DIMENSIONS,
-                'size_units' => [ 'px', '%' ],
+            'profile_user_image_padding',
+            [
+                'label' => __('Image Padding', 'elemental-membership'),
+                'type' => Controls_Manager::DIMENSIONS,
+                'size_units' => ['px', '%'],
                 'default' => [
                     'unit' => 'px',
                     'top' => '0',
                     'right' => '20',
                     'bottom' => '0',
-                    'left' => '20'
-				],
-				'selectors' => [
-					'{{WRAPPER}} .em-user-avatar' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-				],
-			]
-		);
+                    'left' => '20',
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .em-user-avatar' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
+            ]
+        );
 
-		$this->end_controls_section();
-		
-		$this->start_controls_section(
+        $this->end_controls_section();
+
+        $this->start_controls_section(
             'em_profile_header_text_style',
             [
-                'label' => __( 'Header Text', 'elemental-membership' ),
+                'label' => __('Header Text', 'elemental-membership'),
                 'tab' => \Elementor\Controls_Manager::TAB_STYLE,
             ]
-		);
+        );
 
-		$this->add_group_control(
-			Group_Control_Typography::get_type(),
-			[
+        $this->add_group_control(
+            Group_Control_Typography::get_type(),
+            [
                 'name' => 'profile_header_name_typography',
                 'label' => __('Profile Name Typography', 'elemental-membership'),
-				'selector' => '{{WRAPPER}} .em-profile-identifier',
-				'scheme' => Schemes\Typography::TYPOGRAPHY_3,
-			]
+                'selector' => '{{WRAPPER}} .em-profile-identifier',
+                'scheme' => Schemes\Typography::TYPOGRAPHY_3,
+            ]
         );
-        
+
         $this->add_group_control(
-			Group_Control_Typography::get_type(),
-			[
+            Group_Control_Typography::get_type(),
+            [
                 'name' => 'profile_header_actions_typography',
                 'label' => __('Profile Actions Typography', 'elemental-membership'),
-				'selector' => '{{WRAPPER}} .em-header-actions li a',
-				'scheme' => Schemes\Typography::TYPOGRAPHY_3,
-			]
-		);
-		
-		$this->end_controls_section();
-
-		$this->start_controls_section(
-            'em_profile_header_image_style',
-            [
-                'label' => __( 'Profile Image', 'elemental-membership' ),
-                'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+                'selector' => '{{WRAPPER}} .em-header-actions li a',
+                'scheme' => Schemes\Typography::TYPOGRAPHY_3,
             ]
-		);
-
-		$this->end_controls_section();
-
-		$this->start_controls_section(
-            'em_profile_header_logout_style',
-            [
-                'label' => __( 'Logout Link', 'elemental-membership' ),
-                'tab' => \Elementor\Controls_Manager::TAB_STYLE,
-            ]
-		);
-
-		$this->add_group_control(
-			Group_Control_Typography::get_type(),
-			[
-                'name' => 'profile_header_logout_typography',
-                'label' => __('Logout Link Typography', 'elemental-membership'),
-				'selector' => '{{WRAPPER}} .em-logout-btn',
-				'scheme' => Schemes\Typography::TYPOGRAPHY_3,
-			]
-		);
-		
-		$this->add_control(
-			'profile_header_logout_color',
-			[
-				'label' => __( 'Logout Link Color', 'elemental-membership' ),
-				'type' => Controls_Manager::COLOR,
-				'selectors' => [
-					'{{WRAPPER}} .em-logout-btn' => 'color: {{VALUE}};',
-				],
-				'scheme' => [
-					'type' => Schemes\Color::get_type(),
-					'value' => Schemes\Color::COLOR_3,
-				],
-			]
         );
 
-		$this->end_controls_section();
+        $this->end_controls_section();
+
+        $this->start_controls_section(
+            'em_profile_header_image_style',
+            [
+                'label' => __('Profile Image', 'elemental-membership'),
+                'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        $this->end_controls_section();
+
+        $this->start_controls_section(
+            'em_profile_header_logout_style',
+            [
+                'label' => __('Logout Link', 'elemental-membership'),
+                'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        $this->add_group_control(
+            Group_Control_Typography::get_type(),
+            [
+                'name' => 'profile_header_logout_typography',
+                'label' => __('Logout Link Typography', 'elemental-membership'),
+                'selector' => '{{WRAPPER}} .em-logout-btn',
+                'scheme' => Schemes\Typography::TYPOGRAPHY_3,
+            ]
+        );
+
+        $this->add_control(
+            'profile_header_logout_color',
+            [
+                'label' => __('Logout Link Color', 'elemental-membership'),
+                'type' => Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .em-logout-btn' => 'color: {{VALUE}};',
+                ],
+                'scheme' => [
+                    'type' => Schemes\Color::get_type(),
+                    'value' => Schemes\Color::COLOR_3,
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
 
     }
 
     protected function render(){
-        $settings = $this -> get_settings_for_display();
+        $settings = $this->get_settings_for_display();
         $current_user = wp_get_current_user();
-    ?>
+        ?>
 
         <div class="em-profile-header">
 
@@ -275,19 +287,22 @@ class Profile_Header extends Widget_Base{
                     <div class="elementor-row em-row">
 
                         <div class="em-user-avatar">
-                            <?php echo get_avatar( get_the_author_meta('email'), '60' );  ?>
+                            <?php echo get_avatar(get_the_author_meta('email'), '60'); ?>
                         </div>
-                        
+
                         <div class="em-profile-modifier-actions">
 
                             <h2 class="em-profile-identifier">
-                                <?php printf(esc_html($current_user->user_firstname . " " . $current_user->user_lastname)); ?>
+                                <?php printf(esc_html($current_user->user_firstname . " " . $current_user->user_lastname));?>
                             </h2>
 
+						<?php if ($settings['show_profile_action_menu']): ?>
                             <ul class="em-list em-header-actions">
-                                <li><a href="#">Edit Profile</a></li>
-                                <li><a href="#">Settings</a></li>
+                                <li><a href="#"><?php __("Edit Profile", "elemental-membership"); ?></a></li>
+                                <li><a href="#"><?php __("Settings", "elemental-membership"); ?></a></li>
                             </ul>
+						<?php endif;?>
+
                         </div>
 
                         <div class="em-profile-header-nav">
@@ -296,25 +311,26 @@ class Profile_Header extends Widget_Base{
                                 <li><a href="#">Posts</a></li>
                             </ul>
 
-                            <a href="<?php echo wp_logout_url(); ?>" class="em-link-btn em-logout-btn">
-                            Logout
-                            </a>
+							<?php if ($settings['show_logout_link']): ?>
+								<a href="<?php echo wp_logout_url(); ?>" class="em-link-btn em-logout-btn">
+									<?php __("Logout", "elemental-membership"); ?>
+								</a>
+							<?php endif;?>
                         </div>
-                    
+
                     </div>
 
                 </div>
 
             </div>
-        
+
         </div>
 
     <?php
-    }
+}
 
     protected function _content_template(){
-        
-    }
 
+    }
 
 }
