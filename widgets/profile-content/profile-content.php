0ad93d87804c58b7bcfc3091249aac0695fdcb77
Cesar Correchel <cesarcorrechel@gmail.com>
2020-10-28 23:05:39 -0600

    add profile content widget

---
+++ widgets/profile-content/profile-content.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace ElementalMembership\Widgets\ProfileContent;
+
+use Elementor\Controls_Manager;
+use Elementor\Group_Control_Typography;
+use Elementor\Widget_Base;
+
+class Profile_Content extends Widget_Base{
+
+    public function get_name(){
+        return 'em-profile-content';
+    }
+
+    public function get_title(){
+        return __('Profile Content', 'elemental-membership');
+    }
+
+    public function get_icon(){
+        return 'fa fa-form';
+    }
+
+    public function get_categories(){
+        return ['elemental-membership-category'];
+    }
+
+    public function get_keywords() {
+		return [ 'content' ];
+	}
+
+    protected function _register_controls(){
+
+    }
+
+    protected function render(){
+
+    }
+
+    protected function _content_template(){
+        
+    }
+
+}
