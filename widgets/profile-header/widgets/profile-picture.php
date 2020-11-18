11d86f957a2fde196b9ed5b97ee863467de51e66
Cesar Correchel <cesarcorrechel@gmail.com>
2020-11-17 23:09:56 -0600

    add basic rendering of the profile pic

--- widgets/profile-header/widgets/profile-picture.php
+++ widgets/profile-header/widgets/profile-picture.php
@@ -23,8 +23,65 @@ class Profile_Picture extends Widget_Base{
         return ['elemental-membership-category'];
     }
 
-    protected function _register_controls(){}
-    protected function render(){}
+    protected function _register_controls(){
+
+        $this->start_controls_section(
+            'profile_picture',
+            [
+                'label' => __('Picture', 'elemental-membership'),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        $this->add_control(
+            'profile_picture_radius',
+            [
+                'label' => __('Profile Image Radius', 'elemental-membership'),
+                'type' => Controls_Manager::SLIDER,
+                'size_units' => ['px', '%'],
+                'range' => [
+                    'px' => [
+                        'min' => 0,
+                        'max' => 1000,
+                        'step' => 5,
+                    ],
+                    '%' => [
+                        'min' => 15,
+                        'max' => 100,
+                    ],
+                ],
+                'default' => [
+                    'unit' => 'px',
+                    'size' => 300,
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .em-profile-picture img' => 'border-radius: {{SIZE}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
+
+    }
+
+    protected function render(){
+        $settings = $this->get_settings_for_display();
+    ?>
+
+        <div class="em-profile-picture">
+            <?php echo get_avatar(get_the_author_meta('email'), '60'); ?>
+            <?php if(is_user_logged_in()): ?>
+                <div class="em-user-picture__change">
+                    <div class="em-profile-btn">
+                        <span class="dashicons dashicons-camera"></span>
+                    </div>
+                </div>
+            <?php endif; ?>
+        </div>
+
+    <?php    
+    }
+
     protected function _content_template(){}
 
 }
